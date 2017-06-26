4261611b2408d39b439b31d0925f7fc806667f3b
dutyuzi <jian.ma@dlssa.com>
2017-06-26 12:42:20 +0000

    add luis

---
+++ Dialogs/RootLuisDialog.cs
@@ -0,0 +1,179 @@
+﻿namespace LuisBot.Dialogs
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Web;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Builder.FormFlow;
+    using Microsoft.Bot.Builder.Luis;
+    using Microsoft.Bot.Builder.Luis.Models;
+    using Microsoft.Bot.Connector;
+    using System.Net;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+    using System.Web.Script.Serialization;
+ 
+
+    //https://webchat.botframework.com/embed/Dut-Bot?s=0bm-Av1zeuk.cwA.8pU.tbFKX4x6PrRV6QEba5GsJ0nMkj1D_mq5Xk1e_WSDf4c
+    [LuisModel("2958fbdd-d9a4-4166-88c5-86110e4788b9", "12d6ef7baf01488991b414e029b9e7db")]
+    [Serializable]
+    public class RootLuisDialog : LuisDialog<object>
+    {
+        private const string EntityGeographyCity = "builtin.geography.city";
+
+        private const string EntityCollegeName = "院系";
+        private const string EntityCityName = "省市地区";
+        private const string EntityPersonName = "人物";
+        private const string EntityNumber = "数量";
+
+        private const string EntityAirportCode = "AirportCode";
+
+        private IList<string> titleOptions = new List<string> { "“Very stylish, great stay, great staff”", "“good hotel awful meals”", "“Need more attention to little things”", "“Lovely small hotel ideally situated to explore the area.”", "“Positive surprise”", "“Beautiful suite and resort”" };
+
+        public object MessageBox { get; private set; }
+
+        [LuisIntent("")]
+        [LuisIntent("None")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+
+            IList<string> message = new List<string> { "你在说啥！本Bot还在学习中<(￣ˇ￣)/", "对不起，本Bot大脑短路了", "未知的世界还需要我去探索", "沉迷学习无法自拔<(￣ˇ￣)/", "我也不知道==" };
+            Random rnd = new Random();
+            int number = rnd.Next(0, 5);
+            await context.PostAsync(message[number]);
+            context.Wait(this.MessageReceived);
+
+        }
+        
+      
+        [LuisIntent("打招呼")]
+        public async Task SayHello(IDialogContext context, LuisResult result)
+        {
+            IList<string> message = new List<string> { "你好！我是聪明的大工Bot(*^__^*)", "hi(*^__^*)", "你好呀(*^__^*)", "我是大工Bot(*^__^*)，有什么可以帮您？", "hi，亲爱的小伙伴(*^__^*)" };
+            Random rnd = new Random();
+            int number = rnd.Next(0, 5);
+            await context.PostAsync(message[number]);
+            context.Wait(this.MessageReceived);
+        }
+
+
+        [LuisIntent("查询录取分数线")]
+        public async Task Search(IDialogContext context, IAwaitable<IMessageActivity> activity, LuisResult result)
+        {
+            var message = await activity;
+
+            EntityRecommendation YX;
+            EntityRecommendation CITY;
+            if (result.TryFindEntity(EntityCollegeName, out YX))
+            {
+                if(result.TryFindEntity(EntityCityName, out CITY))
+                await context.PostAsync($"'{CITY.Entity}'的 '{YX.Entity}'的录取分数线是555");
+            }
+
+        
+
+        }
+
+        [LuisIntent("查询人数")]
+        public async Task SearchNumber(IDialogContext context, IAwaitable<IMessageActivity> activity, LuisResult result)
+        {
+            var message = await activity;
+
+            EntityRecommendation People;
+         
+            if (result.TryFindEntity(EntityPersonName, out People))
+            {
+
+                await context.PostAsync(QNaMaker("有多少" + People.Entity));
+            }
+            else
+            {
+
+            }
+
+        }
+
+        [LuisIntent("查询人物")]
+        public async Task SearchPerson(IDialogContext context, IAwaitable<IMessageActivity> activity, LuisResult result)
+        {
+            var message = await activity;
+
+            EntityRecommendation People;
+
+            if (result.TryFindEntity(EntityPersonName, out People))
+            {
+
+                await context.PostAsync(QNaMaker(People.Entity+"是谁"));
+                await context.PostAsync((People.Entity));
+            }
+            else
+            {
+
+            }
+
+        }
+
+        [LuisIntent("Help")]
+        public async Task Help(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("Hi! Try asking me things like 'search hotels in Seattle', 'search hotels near LAX airport' or 'show me the reviews of The Bot Resort'");
+
+            context.Wait(this.MessageReceived);
+        }
+
+        private string  QNaMaker(string Query)
+        {
+            var query = Query; //User Query
+            var knowledgebaseId = "d9c89203-5df6-42ed-b37e-4ed0b153e573"; // Use knowledge base id created.
+            var qnamakerSubscriptionKey = "c29fc7e890934924865f4b5f45338282"; //Use subscription key assigned to you.
+            string responseString = string.Empty;
+            //Build the URI
+            Uri qnamakerUriBase = new Uri("https://westus.api.cognitive.microsoft.com/qnamaker/v2.0");
+            var builder = new UriBuilder($"{qnamakerUriBase}/knowledgebases/{knowledgebaseId}/generateAnswer");
+
+            //Add the question as part of the body
+            var postBody = $"{{\"question\": \"{query}\"}}";
+
+            //Send the POST request
+            using (WebClient client = new WebClient())
+            {
+                //Set the encoding to UTF8
+                client.Encoding = System.Text.Encoding.UTF8;
+                //Add the subscription key header
+                client.Headers.Add("Ocp-Apim-Subscription-Key", qnamakerSubscriptionKey);
+                client.Headers.Add("Content-Type", "application/json");
+                responseString = client.UploadString(builder.Uri, postBody);
+                //await context.PostAsync(responseString);
+                int index = responseString.IndexOf("\"answer\"");
+                string str = responseString.Substring(index + 10);
+                int index1 = str.IndexOf("\"");
+                string str1 = str.Substring(0, index1);
+
+                return str1;
+           
+                /* 
+                 * 反序列化总是为空 暂时没找到原因
+                //De-serialize the response
+                QnAMakerResult response;
+                try
+                {
+                    response = JsonConvert.DeserializeObject<QnAMakerResult>(responseString);
+                    //throw new Exception(response.Answer);
+                }
+                catch
+                {
+                    throw new Exception("Unable to deserialize QnA Maker response string.");
+                }
+
+                await context.PostAsync(response.Answer);
+                */
+
+            }
+
+        }
+
+       
+    }
+}
---
+++ QnAMakerResult.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace LuisBot
+{
+   
+    public class QnAMakerResult
+    {
+        /// The top answer found in the QnA Service.
+        /// </summary>
+        [JsonProperty(PropertyName = "answer")]
+
+        public string Answer { get  ; set ; }
+
+        /// <summary>
+        /// The score in range [0, 100] corresponding to the top answer found in the QnA    Service.
+        /// </summary>
+        [JsonProperty(PropertyName = "score")]
+
+        public double Score { get; set; }
+
+    }
+
+}
